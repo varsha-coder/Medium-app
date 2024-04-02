116f8c85ce2dfb2259025f38f0714b5f36ca1121
NAGAVARSHINI R K <nvarshinirk@gmail.com>
2024-04-02 09:27:53 +0530

    added frontend with signup page

---
+++ frontend/src/Components/Quote.tsx
@@ -0,0 +1,18 @@
+export const Quote=()=>{
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+
+     <div className="flex justify-center">
+        <div className="max-w-lg text-center text-3xl font-bold">
+   "The customer service I received was exceptional . The support team went above and beyond to address my concern"
+        </div>
+        
+     </div>
+     <div className="max-w-md text-center text-xl font semibold">
+            Julies Winfield
+        </div>
+        <div className="max-w-md text-center text-md text-slate-400 font semibold">
+           CEO|Acne corp
+        </div>
+    </div>
+
+}
